fff7552d76dc63ae10895db0251ae44eb8488b2e
Chengxin Zhao <zchenxing2002@gmail.com>
2025-02-27 11:17:17 +0100

    Initial Commit

---
+++ Cyanea/CyaneaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyaneaApp.swift
+//  Cyanea
+//
+//  Created by Chengxin Zhao on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CyaneaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CyaneaTests/CyaneaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CyaneaTests.swift
+//  CyaneaTests
+//
+//  Created by Chengxin Zhao on 27/02/2025.
+//
+
+import Testing
+@testable import Cyanea
+
+struct CyaneaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
